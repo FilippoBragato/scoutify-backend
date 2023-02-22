50d4b2f90c68f56321dfbcf3693a42741942983d
Filippo Bragato <filo.bragato@gmail.com>
2023-02-22 17:44:53 +0100

    Edit cors

--- scoutify/settings.py
+++ scoutify/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# CORS_ORIGIN_WHITELIST = [
-#      'http://localhost:3000'
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'https://scoutify-6374f.web.app/'
+]
 CORS_ORIGIN_ALLOW_ALL = True
